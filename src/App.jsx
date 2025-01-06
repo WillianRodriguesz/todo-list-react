68536cc7bc037d4093031784362bf6a6c1ac8321
Willian Rodrigues <willianrdriguesz@gmail.com>
2025-01-06 17:50:59 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
+
+function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar programação",
+      description: "Estudar programação React",
+      isCompleted: false,
+    },
+
+    {
+      id: 2,
+      title: "Treinar",
+      description: "Treinar musculação",
+      isCompleted: false,
+    },
+  ]);
+
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de tarefas
+        </h1>
+        <Tasks tasks={tasks} />
+        <AddTask />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,23 @@
+import { ChevronRightIcon } from "lucide-react";
+
+function Tasks(props) {
+  console.log(props);
+
+  return (
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+      <h1></h1>
+      {props.tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+          <button className="bg-slate-400 w-full p-2 rounded-md text-left text-white ">
+            {task.title}
+          </button>
+          <button className="bg-slate-400 p-2 rounded-md text-white ">
+            <ChevronRightIcon />
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default Tasks;
