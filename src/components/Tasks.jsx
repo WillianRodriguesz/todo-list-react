552f5f89ecdc63005bc941d7514c3610baceac5b
Willian Rodrigues <willianrdriguesz@gmail.com>
2025-01-07 11:43:10 -0300

    feat(task): Validando task feita e excluindo task

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,18 +1,27 @@
-import { ChevronRightIcon } from "lucide-react";
+import { ChevronRightIcon, Trash2Icon } from "lucide-react";
 
 function Tasks(props) {
-  console.log(props);
-
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       <h1></h1>
       {props.tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
-          <button className="bg-slate-400 w-full p-2 rounded-md text-left text-white ">
+          <button
+            onClick={() => props.onTaskClick(task.id)}
+            className={`bg-slate-400 w-full p-2 rounded-md text-left text-white
+            ${task.isCompleted && "line-through"}`}
+          >
             {task.title}
           </button>
           <button className="bg-slate-400 p-2 rounded-md text-white ">
             <ChevronRightIcon />
+          </button>
+
+          <button
+            onClick={() => props.deleteTaskClick(task.id)}
+            className="bg-slate-400 p-2 rounded-md text-white "
+          >
+            <Trash2Icon />
           </button>
         </li>
       ))}
