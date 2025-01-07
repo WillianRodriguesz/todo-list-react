e92b7defb2dcdf0353ef61b6df68f2809aa70f8a
Willian Rodrigues <willianrdriguesz@gmail.com>
2025-01-07 14:34:34 -0300

    feat(AddTask): Adicionando novas task no todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
 import { useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
+import { v4 } from "uuid";
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      title: "Estudar programação",
-      description: "Estudar programação React",
-      isCompleted: false,
-    },
-
-    {
-      id: 2,
-      title: "Treinar",
-      description: "Treinar musculação",
-      isCompleted: false,
-    },
-  ]);
+  const [tasks, setTasks] = useState([]);
 
   function onTaskClick(taskId) {
     const newTasks = tasks.map((task) => {
@@ -36,18 +23,29 @@ function App() {
     setTasks(newTasks);
   }
 
+  function addTaskSubmit(title, description) {
+    const newTask = {
+      id: v4(),
+      title,
+      description,
+      isCompleted: false,
+    };
+
+    setTasks([...tasks, newTask]);
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px]">
+      <div className="w-[500px] space-y-4">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciador de tarefas
         </h1>
+        <AddTask addTaskSubmit={addTaskSubmit} />
         <Tasks
           tasks={tasks}
           onTaskClick={onTaskClick}
           deleteTaskClick={deleteTaskClick}
         />
-        <AddTask />
       </div>
     </div>
   );
